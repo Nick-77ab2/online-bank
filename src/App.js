89620c292976cb7d4b07c4dcad232a4f80c6fe3b
Nick <hopogoogly@gmail.com>
2024-08-06 15:54:56 -0400

    Worked on Front-end visuals and login/signup page. No functionality yet. Fixed some package issues.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './App.css';
-import MessageComponent from './components/MessageComponent';
+import BankName from './components/BankName';
+import { LoginSignup } from './components/LoginSignup/LoginSignup';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <MessageComponent />
-            </header>
+            <div className="App-logo">
+            <BankName />
+            </div>
+            {/*<header className="App-header">
+            <MessageComponent />
+            </header>*/}
+            <LoginSignup />
+            {/*fun stuff to add*/}
+            {/*<button onClick={handleLogin}>Login</button>*/}
+            {/*<button onClick={handleLogout}>Logout</button>*/}
+            {/*<button onClick={handleRegister}>Register</button>*/}
+            {/*<button onClick={handleForgotPassword}>Forgot Password</button>*/}
+            {/*<button onClick={handleResetPassword}>Reset Password</button>*/}
+            {/*<button onClick={handleUpdateProfile}>Update Profile</button>*/}
+            {/*<button onClick={handleChangePassword}>Change Password</button>*/}
+            {/*<button onClick={handleViewTransactions}>View Transactions</button>*/}
+            {/*<button onClick={handleDepositFunds}>Deposit Funds</button>*/}
+            {/*<button onClick={handleWithdrawFunds}>Withdraw Funds</button>*/}
+            {/*<button onClick={handleTransferFunds}>Transfer Funds</button>*/}
+            {/*<button onClick={handleViewAccountDetails}>View Account</button>*/}
+            <footer> Created by Nick Pelletier, 2024</footer>
+            <footer>v0.1 Dev</footer>
         </div>
     );
 }
---
+++ src/components/BankName.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BankName = () => {
+    return <h1>Nick&apos;s Test Bank</h1>;
+}
+
+export default BankName;
