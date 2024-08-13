4473bfb11878ef7c2b2e28a3f5da234b671355d0
Nick <hopogoogly@gmail.com>
2024-08-13 14:05:03 -0400

    Worked on the backend mostly. Frontend commit mainly involves unpacking the token and sending the result to console so I can debug and understand the token and its content.

--- src/Account.js
+++ src/Account.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 export const Account = () => {
+    const parseJWT = () => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const base64Url = token.split('.')[1];
+            const base64 = base64Url.replace('-', '+').replace('_', '/');
+            return JSON.parse(atob(base64));
+        }
+        return null;
+    }
+
+    const user = parseJWT();
+    console.log("Information in user: ",user);
     return (
         <div>
             <h1>Account Information</h1>
